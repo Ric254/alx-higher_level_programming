bf8720cb48bdcc6bc06fab33e1441e935ac6df69
Ric254 <eerickmwangi@gmail.com>
2023-03-13 14:27:02 -0700

    javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0, len = myArray.length; i < len; i++) {
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < myArray.length; i++) {
   console.log(myArray[i]);
 }
